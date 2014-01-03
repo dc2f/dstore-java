7fc5827e0f1c98daddff9aec2ccfd60155d2d349
Florian Gutmann <f.gutmann@chronimo.com>
2014-01-03 20:04:47 +0100

    started implementing test framework and tests
    - added recursive tree assertions and nice ExpectedTree builder api
    - implemented first general test

---
+++ src/test/java/com/dc2f/dstore/test/TreeAssertions.java
@@ -0,0 +1,135 @@
+package com.dc2f.dstore.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.dc2f.dstore.hierachynodestore.WorkingTreeNode;
+import com.dc2f.dstore.storage.Property;
+import com.dc2f.dstore.storage.StorageId;
+
+/**
+ * Functions for asserting working trees.
+ */
+public class TreeAssertions {
+
+	/**
+	 * Asserts a working tree node to equal the expected node.
+	 * 
+	 * @param expected The expected node.
+	 * @param node The node to assert.
+	 * @return A new ExpectedNode as a copy of the current ExpectedNode but filled in with all StorageIds of the asserted node.
+	 */
+	public static ExpectedNode assertTree(ExpectedNode expected, WorkingTreeNode node) {
+		return assertTree(null, expected, node);
+	}
+	
+	/**
+	 * Asserts a working tree node to equal the expected node.
+	 * 
+	 * @param message The assertion message.
+	 * @param expected The expected node.
+	 * @param node The node to assert.
+	 * @return The asserted tree including all ids.
+	 */
+	public static ExpectedNode assertTree(String message, ExpectedNode expected, WorkingTreeNode node) {
+		message = message == null ? "" : message + "; ";
+		
+		// check the properties
+		assertEquals(message + "Expected properties count equals actual properties count.", 
+				(long) (expected.properties.length / 2), node.getProperties().size());
+		
+		for(int i=0; i < expected.properties.length; i+=2) {
+			String key = (String) expected.properties[i];
+			Object expectedValue = (Object) expected.properties[i+1];
+			
+			Property actual = node.getProperty(key);
+			assertNotNull(actual);
+			assertEquals(expectedValue, actual.getObjectValue());
+		}
+		
+		// check children
+		assertEquals(message + "Expected children count equals actual children count",
+				expected.children.size(), node.getChildrenCount());
+
+		Iterator<ExpectedNode> expectedChildren = expected.children.iterator();
+		Iterator<WorkingTreeNode> actualChildren = node.getChildren().iterator();
+		
+		List<ExpectedNode> childrenWithId = new ArrayList<>(node.getChildrenCount());
+		for(int i=0; i<node.getChildrenCount(); i++) {
+			ExpectedNode assertedChild = assertTree(message, expectedChildren.next(), actualChildren.next());
+			childrenWithId.add(assertedChild);
+		}
+
+		return new ExpectedNode(node.getStorageId(), expected.properties, childrenWithId);
+	}
+	
+	/**
+	 * The expected version of a working tree node.
+	 */
+	public static class ExpectedNode {
+		
+		public ExpectedNode(StorageId storageId, Object[] properties, List<ExpectedNode> children) {
+			this.storageId = storageId;
+			this.properties = properties;
+			this.children = new ArrayList<>(children);
+		}
+
+		/**
+		 * The expected storage id
+		 */
+		public final StorageId storageId;
+		
+		/**
+		 * The expected properties in key value format.
+		 * Every odd parameter is a key and every even a value.
+		 */
+		public final Object [] properties;
+		
+		/**
+		 * The expected children of the node.
+		 */
+		public final List<ExpectedNode> children;
+	}
+	
+	/**
+	 * Creates an expected node with properties but without children.
+	 * 
+	 * @param properties 
+	 *		The expected properties in key value format.
+	 *		Every odd parameter is a key and every even a value.
+	 */
+	public static ExpectedNode node(Object[] properties) {
+		return new ExpectedNode(null, properties, new LinkedList<ExpectedNode>());
+	}
+	
+	/**
+	 * Creates an expected node with properties and children.
+	 * 
+	 * @param properties 
+	 *		The expected properties in key value format.
+	 *		Every odd parameter is a key and every even a value.
+	 * @param children
+	 * 		The child nodes of the node.
+	 */
+	public static ExpectedNode node(Object[] properties, ExpectedNode... children) {
+		return new ExpectedNode(null, properties, Arrays.asList(children));
+	}
+	
+	/**
+	 * Creates a properties object to use in combination with the {@link #node(Object[], List)} function.
+	 * 
+	 * @param properties 
+	 * 				The expected properties in key value format.
+	 * 				Every odd parameter is a key and every even a value.
+	 * @return Object property array.
+	 */
+	public static Object [] properties(Object... properties) {
+		return properties;
+	}
+}
---
+++ src/test/java/com/dc2f/dstore/test/hierachynodestore/HierarchicalNodeStoreTest.java
@@ -0,0 +1,76 @@
+package com.dc2f.dstore.test.hierachynodestore;
+
+import static com.dc2f.dstore.test.TreeAssertions.assertTree;
+import static com.dc2f.dstore.test.TreeAssertions.node;
+import static com.dc2f.dstore.test.TreeAssertions.properties;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.dc2f.dstore.hierachynodestore.HierarchicalNodeStore;
+import com.dc2f.dstore.hierachynodestore.WorkingTree;
+import com.dc2f.dstore.hierachynodestore.WorkingTreeNode;
+import com.dc2f.dstore.storage.StorageBackend;
+import com.dc2f.dstore.storage.flatjsonfiles.SlowJsonFileStorageBackend;
+import com.dc2f.dstore.test.TreeAssertions.ExpectedNode;
+import com.dc2f.utils.FileUtils;
+
+public class HierarchicalNodeStoreTest {
+
+	private HierarchicalNodeStore nodeStore;
+	private File jsonStorageFolder;
+	
+	@Before
+	public void setupDstore() throws IOException {
+		jsonStorageFolder = Files.createTempDirectory("dstore-json-test-storage").toFile();
+		StorageBackend storageBackend = new SlowJsonFileStorageBackend(jsonStorageFolder);
+		nodeStore = new HierarchicalNodeStore(storageBackend);
+	}
+	
+	@After
+	public void destroyDstore() throws IOException {
+		if(jsonStorageFolder != null && jsonStorageFolder.exists()) {
+			FileUtils.removeRecursive(jsonStorageFolder.toPath());
+		}
+	}
+	
+	@Test
+	public void testWorkingTreeVersions() {
+		WorkingTree wt1 = nodeStore.checkoutBranch("master");
+		WorkingTreeNode root1 = wt1.getRootNode();
+
+		ExpectedNode onlyRoot = node(properties("name", ""));
+
+		assertTree("wt1 must only see the root node", onlyRoot, root1);
+		
+		WorkingTreeNode a = root1.addChild("A");
+		WorkingTreeNode b = root1.addChild("B");
+		
+		ExpectedNode expectedAB = node(properties("name", ""),
+				node(properties("name", "A")),
+				node(properties("name", "B"))
+			);
+		
+		assertTree("Changes are visible inside uncommited working tree wt1", expectedAB, root1);
+		assertTrue("Get rootNode() must always return the same root node", root1 == wt1.getRootNode());
+		
+		WorkingTree wt2 = nodeStore.checkoutBranch("master");
+		WorkingTreeNode root2 = wt2.getRootNode();
+		assertTree("Changes are not visible in wt2 which was checked out before commiting wt1", onlyRoot, root2);
+
+		wt1.commit("Commiting wt1, nothing must have changed in already existing working trees");
+		assertTree("root1 data didn't change during commit", expectedAB, root1);
+		assertTrue("Get rootNode() must return the same root node as before commiting", root1 == wt1.getRootNode());
+		
+		WorkingTree wt3 = nodeStore.checkoutBranch("master");
+		WorkingTreeNode root3 = wt3.getRootNode();
+		
+		assertTree("wt3 must see changes commited by wt1", expectedAB, root3);
+	}
+}

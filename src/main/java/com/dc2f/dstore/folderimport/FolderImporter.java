3c1aa982b2f3c188d48aa7a3b2346e3aa66995ae
Florian Gutmann <f.gutmann@chronimo.com>
2014-01-08 19:44:21 +0100

    removed system.out.println statements and main method in FolderImporter

--- src/main/java/com/dc2f/dstore/folderimport/FolderImporter.java
+++ src/main/java/com/dc2f/dstore/folderimport/FolderImporter.java
@@ -2,7 +2,6 @@ package com.dc2f.dstore.folderimport;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,13 +11,13 @@ import java.util.Iterator;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.dc2f.dstore.hierachynodestore.HierarchicalNodeStore;
 import com.dc2f.dstore.hierachynodestore.WorkingTree;
 import com.dc2f.dstore.hierachynodestore.WorkingTreeNode;
-import com.dc2f.dstore.hierachynodestore.WorkingTreeUtils;
 import com.dc2f.dstore.storage.Property;
-import com.dc2f.dstore.storage.map.HashMapStorage;
 import com.google.common.base.Charsets;
 
 /**
@@ -28,10 +27,11 @@ public class FolderImporter {
 	
 	private HierarchicalNodeStore nodeStore;
 
+	private static Logger LOG = LoggerFactory.getLogger(FolderImporter.class);
+	
 	public FolderImporter(HierarchicalNodeStore nodeStore) {
 		this.nodeStore = nodeStore;
 	}
-	
 	
 	public void startImport(File rootFolder) {
 		try {
@@ -40,8 +40,6 @@ public class FolderImporter {
 			final Path rootPath = rootFolder.getAbsoluteFile().toPath();
 
 			Files.walkFileTree(rootPath, new SimpleFileVisitor<Path>() {
-				
-				private WorkingTreeNode currentFolder;
 				
 				private WorkingTreeNode getNode(Path path) {
 					Path relativePath = rootPath.relativize(path);
@@ -56,7 +54,6 @@ public class FolderImporter {
 						if (childrenIterator.hasNext()) {
 							node = childrenIterator.next();
 						} else {
-							//throw new IllegalStateException("Unable to find child {" + name + "} for path {" + path + "}");
 							node = node.addChild(name);
 						}
 						
@@ -65,20 +62,14 @@ public class FolderImporter {
 				}
 				
 				@Override
-				public FileVisitResult preVisitDirectory(Path dir,
-						BasicFileAttributes attrs) throws IOException {
-					
-//					return super.preVisitDirectory(dir, attrs);
-					currentFolder = getNode(dir);
-					
+				public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
 					return FileVisitResult.CONTINUE;
 				}
 				
 				@Override
-				public FileVisitResult visitFile(Path file,
-						BasicFileAttributes attrs) throws IOException {
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
 					WorkingTreeNode fileNode = getNode(file);
-					System.out.println("Loaded file  contents for {" + file + "}: " + WorkingTreeUtils.debugRecursiveTree(fileNode));
+					
 					if (file.toString().endsWith(".json")) {
 						try {
 							JSONObject jsonObject = new JSONObject(com.google.common.io.Files.toString(file.toFile(), Charsets.UTF_8));
@@ -88,7 +79,7 @@ public class FolderImporter {
 								fileNode.setProperty(propertyName, new Property(jsonObject.get(propertyName)));
 							}
 						} catch (JSONException e) {
-							throw new RuntimeException("unable to read json.", e);
+							throw new RuntimeException("unable to read json of file " + file.toString(), e);
 						}
 					} else {
 						fileNode.setProperty("data", new Property(com.google.common.io.Files.toString(file.toFile(), Charsets.UTF_8)));
@@ -97,24 +88,10 @@ public class FolderImporter {
 				}
 			});
 			
-			System.out.println("Imported");
 			masterBranch.commit(null);
-			System.out.println(WorkingTreeUtils.debugRecursiveTree(rootNode));
-
+			LOG.debug("Imported folder {}", rootFolder.getName());
 		} catch (IOException e) {
 			throw new RuntimeException("Error while importing path.", e);
 		}
 	}
-
-	public static void main(String[] args) {
-		if (args.length < 1) {
-			System.out.println("use exampledata/testimport");
-			System.exit(1);
-		}
-		HierarchicalNodeStore nodeStore = new HierarchicalNodeStore(new HashMapStorage());
-		FolderImporter importer = new FolderImporter(nodeStore);
-		importer.startImport(new File(args[0]));
-		
-	}
-
 }
